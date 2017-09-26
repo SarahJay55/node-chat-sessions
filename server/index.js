f5ac08c5dd0b6922fe8604ce4ada6228a98e8219
SarahJay55 <30481405+SarahJay55@users.noreply.github.com>
2017-09-26 12:28:07 -0700

    Mini Project Node-Chat

--- server/index.js
+++ server/index.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const mc = require( `${__dirname}/controllers/messages_controller` );
+const createInitialSession = require('./middlewares/session.js');
+const filter = require("./middlewares/filter");
+
 
 const app = express();
 
+app.use( session({
+    secret: "lskdjfiefal",
+    resave: false, //whether to re-save the cookie/overwite every contact
+    saveUninitialized: false, //add a cookie even if node thinks we don't need one
+    cookie: { maxAge: 10000 } //how long until cookie expires
+}));
+
+app.use(createInitialSession);
+
 app.use( bodyParser.json() );
+
+app.use(function(req, res, next){
+    if(req.method === "POST" || req.method === "PUT"){
+        filter(req, res, next);
+    } else {
+        next();
+    }
+})
+
+
 app.use( express.static( `${__dirname}/../public/build` ) );
+
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
+app.get( messagesBaseUrl + '/history', mc.history );
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
 
